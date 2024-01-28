7484701bd03ff18c2e972749404fe539fdacf03f
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-28 01:16:29 -0500

    init

---
+++ src/main/java/com/insurgencedev/rivalhoesaddon/RivalHoesAddon.java
@@ -0,0 +1,17 @@
+package com.insurgencedev.rivalhoesaddon;
+
+import com.insurgencedev.rivalhoesaddon.listeners.RivalHoesEventListeners;
+import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
+import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
+import org.insurgencedev.insurgenceboosters.libs.fo.Common;
+
+@IBoostersAddon(name = "RivalHoesAddon", version = "1.0.0", author = "InsurgenceDev", description = "RivalHarvesterHoes Support")
+public class RivalHoesAddon extends InsurgenceBoostersAddon {
+
+    @Override
+    public void onAddonReloadAblesStart() {
+        if (Common.doesPluginExist("RivalHarvesterHoes")) {
+            registerEvent(new RivalHoesEventListeners());
+        }
+    }
+}
---
+++ src/main/java/com/insurgencedev/rivalhoesaddon/listeners/RivalHoesEventListeners.java
@@ -0,0 +1,34 @@
+package com.insurgencedev.rivalhoesaddon.listeners;
+
+import me.rivaldev.harvesterhoes.api.events.HoeEssenceReceivePreEnchantEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
+import org.insurgencedev.insurgenceboosters.data.BoosterFindResult;
+
+public final class RivalHoesEventListeners implements Listener {
+
+    @EventHandler
+    private void onReceive(HoeEssenceReceivePreEnchantEvent event) {
+        final String TYPE = "Essence";
+        final String NAMESPACE = "RIVAL_HOES";
+        final double[] totalMulti = {1};
+
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
+        }
+
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
+
+        event.setEssence(calculateAmount(event.getEssence(), totalMulti[0]));
+    }
+
+    private long calculateAmount(double amount, double multi) {
+        return (long) (amount * (multi < 1 ? 1 + multi : multi));
+    }
+
+}
