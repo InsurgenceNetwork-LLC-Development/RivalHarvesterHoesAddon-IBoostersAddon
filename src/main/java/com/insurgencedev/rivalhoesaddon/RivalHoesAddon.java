ac430b810be446a1e376ab79f652560a0082677a
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-30 10:13:01 -0500

    accounts for decimal values and added xp boost

--- src/main/java/com/insurgencedev/rivalhoesaddon/RivalHoesAddon.java
+++ src/main/java/com/insurgencedev/rivalhoesaddon/RivalHoesAddon.java
@@ -5,7 +5,7 @@ import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 import org.insurgencedev.insurgenceboosters.libs.fo.Common;
 
-@IBoostersAddon(name = "RivalHoesAddon", version = "1.0.1", author = "InsurgenceDev", description = "RivalHarvesterHoes Support")
+@IBoostersAddon(name = "RivalHoesAddon", version = "1.0.2", author = "InsurgenceDev", description = "RivalHarvesterHoes Support")
 public class RivalHoesAddon extends InsurgenceBoostersAddon {
 
     @Override
--- src/main/java/com/insurgencedev/rivalhoesaddon/listeners/RivalHoesEventListeners.java
+++ src/main/java/com/insurgencedev/rivalhoesaddon/listeners/RivalHoesEventListeners.java
@@ -1,6 +1,7 @@
 package com.insurgencedev.rivalhoesaddon.listeners;
 
 import me.rivaldev.harvesterhoes.api.events.HoeEssenceReceivePreEnchantEvent;
+import me.rivaldev.harvesterhoes.api.events.HoeXPGainEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.insurgencedev.insurgenceboosters.api.IBoosterAPI;
@@ -29,8 +30,29 @@ public final class RivalHoesEventListeners implements Listener {
         }
     }
 
-    private long calculateAmount(double amount, double multi) {
-        return (long) (amount * (multi < 1 ? 1 + multi : multi));
+    @EventHandler
+    private void onGain(HoeXPGainEvent event) {
+        final String TYPE = "XP";
+        final String NAMESPACE = "RIVAL_HOES";
+        final double[] totalMulti = {0};
+
+        BoosterFindResult pResult = IBoosterAPI.INSTANCE.getCache(event.getPlayer()).getBoosterDataManager().findActiveBooster(TYPE, NAMESPACE);
+        if (pResult instanceof BoosterFindResult.Success boosterResult) {
+            totalMulti[0] += boosterResult.getBoosterData().getMultiplier();
+        }
+
+        IBoosterAPI.INSTANCE.getGlobalBoosterManager().findGlobalBooster(TYPE, NAMESPACE, globalBooster -> {
+            totalMulti[0] += globalBooster.getMultiplier();
+            return null;
+        }, () -> null);
+
+        if (totalMulti[0] > 0) {
+            event.setXP(calculateAmount(event.getXP(), totalMulti[0]));
+        }
+    }
+
+    private double calculateAmount(double amount, double multi) {
+        return amount * (multi < 1 ? 1 + multi : multi);
     }
 
 }
